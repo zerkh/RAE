e749fe9cdf5c9380f8bbde5ea13b387e4b2f9eed
zerkh <mhzx20@163.com>
2015-08-10 08:42:21 +0800

    2015/8/10 8:42

--- Constdef.h
+++ Constdef.h
@@ -12,14 +12,14 @@ const int REC_NODE = 2;
 typedef pair<int, int> span;
 
 //Erec
-static double ALPHA = 0.2;
+static double ALPHA = 0.002;
 //Ereo
 static double BETA = 0.01;
 //Edis
-static double GAMMA = 0;
+static double GAMMA = 0.01;
 //Decay
 static double ZETA = 0.001;
 //learning rate
-static double RATE = 0.2;
+static double RATE = 0.00002;
 
 #endif
